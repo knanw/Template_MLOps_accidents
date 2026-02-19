b66fabd3f961afb0dd28a321875131b729873cb2
knanw <knanw@outlook.com>
2026-02-19 15:26:14 +0100

    Second preprocessing of the data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -70,6 +70,8 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     # df = drop_lines_with_nan_values(df)
 
     # Split data into training and testing sets
+
+    
     X_train, X_test, y_train, y_test = split_data(df)
 
     # Fill NaN values
